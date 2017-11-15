c0c13d363af785c0675a2dcae04d2befe3473c47
Joy Liu <your_email@youremail.com>
2017-11-15 11:15:56 +0800

    create the first project

---
+++ PythonDemo/Learn_log_Django/11_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/joyliu/PycharmProjects/PythonCodeDemo/PythonDemo/Learn_log_Django/11_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
